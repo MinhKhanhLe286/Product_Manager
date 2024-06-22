adbf4b706d45d2439d3c84987672d58e1a3e7962
KhanhMnhLee <minhkhanh250204z@gmail.com>
2024-06-22 21:42:30 +0700

    starrt biuld page admin

--- src/controller/client/productsController.js
+++ src/controller/client/productsController.js
@@ -1,8 +1,22 @@
-index = (req, res) => {
-  myObject = {
-    title: "Products",
-  };
-  res.render("./client/pages/products/index.pug", myObject);
+const ProductModel = require("../../model/productModel");
+
+const index = async (req, res) => {
+  try {
+    const products = await ProductModel.find({
+      deleted: false,
+      status: "active",
+    });
+
+    console.log(products);
+    const myObject = {
+      title: "Products",
+      products: products,
+    };
+    res.render("./client/pages/products/index.pug", myObject);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 module.exports = {
